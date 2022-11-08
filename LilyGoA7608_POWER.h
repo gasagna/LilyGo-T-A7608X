27d6773137e8bd87ea24efc298807140bf9591a5
Davide Lasagna <lasagnadavide@gmail.com>
2022-11-08 19:38:20 +0000

    working version

--- LilyGoA7608_POWER.h
+++ LilyGoA7608_POWER.h
@@ -72,13 +72,14 @@ void enable_battery_functions() {
 // disable battery
 void disable_battery_functions() {
     pinMode(BAT_EN, OUTPUT);
-    digitalWrite(BAT_EN, LOW); delay(100);
+    digitalWrite(BAT_EN, LOW);
 }
 
 // A7608 Reset
 // see simcom-A7608 hardware design document
 void modem_hw_reset() {
     pinMode(RESET, OUTPUT);
+    digitalWrite(RESET, LOW);  delay(100);
     digitalWrite(RESET, HIGH); delay(1000);
     digitalWrite(RESET, LOW);  delay(100);
 }
@@ -87,6 +88,7 @@ void modem_hw_reset() {
 // see simcom-A7608 hardware design document
 void modem_hw_poweron() {
     pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, LOW);  delay(100);
     digitalWrite(PWR_PIN, HIGH); delay(1000); // this is Ton
     digitalWrite(PWR_PIN, LOW);  delay(15000);
 }
@@ -101,41 +103,11 @@ void modem_hw_poweroff() {
 
 
 // avoid modem starting when waking up from sleep
-// avoid battery functions not being available on battery
-void keepPWRPINLOW() {
-    pinMode(PWR_PIN, OUTPUT);
-    digitalWrite(PWR_PIN, LOW); delay(10);
-    gpio_hold_en(GPIO_NUM_4);
-    gpio_deep_sleep_hold_en();
-}
 
 void keepRESETPINLOW() {
     pinMode(RESET, OUTPUT);
-    digitalWrite(RESET, LOW); delay(10);
+    digitalWrite(RESET, LOW);
     gpio_hold_en(GPIO_NUM_5);
-    gpio_deep_sleep_hold_en();
-}
-
-void keepBATENPINLOW() {
-    pinMode(BAT_EN, OUTPUT);
-    digitalWrite(BAT_EN, LOW); delay(10);
-    gpio_hold_en(GPIO_NUM_12);
-    gpio_deep_sleep_hold_en();
-}
-
-void keep_pins_state() {
-    pinMode(PWR_PIN, OUTPUT);
-    // pinMode(RESET,   OUTPUT);
-    pinMode(BAT_EN,  OUTPUT);
-
-    digitalWrite(PWR_PIN, LOW);  delay(10);
-    // digitalWrite(RESET,   LOW);  delay(10);
-    digitalWrite(BAT_EN,  HIGH); delay(10);
-
-    gpio_hold_en(GPIO_NUM_4);
-    // gpio_hold_en(GPIO_NUM_5);
-    gpio_hold_en(GPIO_NUM_12);
-
     gpio_deep_sleep_hold_en();
 }
 
