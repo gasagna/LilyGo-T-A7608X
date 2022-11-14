cb5d4dad89f29e91861aead7c4a6f758e71c8e2b
Davide Lasagna <lasagnadavide@gmail.com>
2022-11-14 21:01:36 +0000

    move pin definitions

--- LilyGoA7608.h
+++ LilyGoA7608.h
@@ -1,6 +1,32 @@
 #ifndef LILYGOA7608_H_
 #define LILYGOA7608_H_
 
+////////////////////////////////////////////////////
+// PINS                                           //
+////////////////////////////////////////////////////
+
+// configuration for serial port to simcom module
+#define PIN_TX   26
+#define PIN_RX   27
+
+// for the battery
+#define BAT_EN   12
+#define BAT_ADC  35 // read battery voltage, Disabled when using USB - ONLY INPUT PIN
+
+// modem pins
+#define PWR_PIN  4
+#define RESET    5
+
+// sd card pins
+#define SD_MISO  2
+#define SD_MOSI  15
+#define SD_SCLK  14
+#define SD_CS    13
+
+////////////////////////////////////////////////////
+// OTHER FUNCTIONALITY                            //
+////////////////////////////////////////////////////
+
 #include "LilyGoA7608_POWER.h"
 #include "LilyGoA7608_MQTT.h"
 
--- LilyGoA7608_POWER.h
+++ LilyGoA7608_POWER.h
@@ -6,17 +6,6 @@
 
 #include "esp_sleep.h"
 
-// configuration for serial port to simcom module
-#define PIN_TX  26
-#define PIN_RX  27
-
-// for the battery
-#define BAT_EN   12
-#define BAT_ADC  35 /* read battery voltage, Disabled when using USB - ONLY INPUT PIN */
-
-// modem pins
-#define PWR_PIN 4
-#define RESET   5
 
 // number of samples for battery reading
 #define NSAMPLES 10
